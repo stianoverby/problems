b6d68d7474922726a0d30b26c755a6b85dc218b0
stianoverby <59914184+stianoverby@users.noreply.github.com>
2021-09-26 14:26:45 +0200

    Create move_zeros.c
    added .c solution

---
+++ C/move_zeros.c
@@ -0,0 +1,26 @@
+/*
+ *Given an integer array nums, move all 0's to the end of it while maintaining the relative    * order of the non-zero elements.
+ *
+ *Note that you must do this in-place without making a copy of the array.
+ *
+ *Constraints:
+ *
+ *1 <= nums.length <= 104
+ *-231 <= nums[i] <= 231 - 1
+ */
+
+void move_zeroes(int* nums, int nums_size)
+{
+    int index_to_insert_non_zero, i;
+    
+    index_to_insert_non_zero = 0;
+    for(i = 0; i< nums_size; i++)
+    {
+        if (nums[i] != 0)
+            nums[index_to_insert_non_zero++] = nums[i];
+    }
+    
+    for(i = index_to_insert_non_zero; i < nums_size; i++)
+        nums[i] = 0;
+}
+
