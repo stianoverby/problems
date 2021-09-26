842c9b6eaa52832c25bafcc628370e848b27a076
stianoverby <59914184+stianoverby@users.noreply.github.com>
2021-09-26 14:30:31 +0200

    Update move_zeros.c
    added comments

--- C/move_zeros.c
+++ C/move_zeros.c
@@ -16,10 +16,17 @@ void move_zeroes(int* nums, int nums_size)
     index_to_insert_non_zero = 0;
     for(i = 0; i< nums_size; i++)
     {
+        /*
+        If non_zero element is found, place it in start of the list, but after all previous
+        moved non-zero elements
+        */
         if (nums[i] != 0)
             nums[index_to_insert_non_zero++] = nums[i];
     }
     
+    /*
+    Set every element after last non-zero element to 0 
+    */
     for(i = index_to_insert_non_zero; i < nums_size; i++)
         nums[i] = 0;
 }
